0a73702cd07452dcb36a24004a7569913f4e2bb6
Aaron-Sauter <mrene@Apple-ThinkPad.fritz.box>
2021-08-04 14:01:41 +0200

    Mintel HD 1000

---
+++ src/main/java/net/mcreator/pcmod/block/MintelHD1000Block.java
@@ -0,0 +1,49 @@
+
+package net.mcreator.pcmod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.pcmod.itemgroup.GrafikkartenItemGroup;
+import net.mcreator.pcmod.PcmodModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@PcmodModElements.ModElement.Tag
+public class MintelHD1000Block extends PcmodModElements.ModElement {
+	@ObjectHolder("pcmod:mintel_hd_1000")
+	public static final Block block = null;
+	public MintelHD1000Block(PcmodModElements instance) {
+		super(instance, 19);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(GrafikkartenItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			setRegistryName("mintel_hd_1000");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
